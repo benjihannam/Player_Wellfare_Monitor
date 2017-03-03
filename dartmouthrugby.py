ad7862f40f05b98a2d75f0cf22ffc00cae1c8976
benjihannam <benjamin.c.hannam.18@dartmouth.edu>
2017-03-03 12:34:52 -0800

    checks if a player is already in the data base before adding

--- dartmouthrugby.py
+++ dartmouthrugby.py
@@ -4,9 +4,15 @@ from firebase import firebase
 firebase = firebase.FirebaseApplication('https://drfc-tracker.firebaseio.com', None)
 
 
+# adds a new player into the data base
 def add_player(firebase, first, last, position):
-	data = {'first_name' : first, 'last_name' : last, 'position' : position}
-	firebase.put("/users", first+"_"+last, data)
+
+	if get_player(firebase, first, last) is None:
+		data = {'first_name' : first, 'last_name' : last, 'position' : position, 'Non Contact' : 0, "Contact" : 0}
+		firebase.put("/users", first+"_"+last, data)
+		print "Added " + first + " " + last + " to the database."
+	else:
+		print "user already in the database"
 
 
 def get_player(firebase, first, last):
@@ -15,6 +21,7 @@ def get_player(firebase, first, last):
 
 
 add_player(firebase, "Benji", "Hannam", "Flanker")
+add_player(firebase, "Steve", "Hinshaw", "Lock")
 add_player(firebase, "Steve", "Hinshaw", "Lock")
 print firebase.get('/users', None, params={'print': 'pretty'}, headers={'X_FANCY_HEADER': 'very fancy'})
 print get_player(firebase, "Benji", "Hannam")
